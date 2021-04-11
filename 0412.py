0fbd8e322f5ba49ef4dc264327cbca372fc60b82
chomingeon 조민건 <66959363+chomingeon@users.noreply.github.com>
2021-04-12 08:55:39 +0900

    Update 0412.py

--- 0412.py
+++ 0412.py
@@ -1,6 +1,13 @@
+##a=123456
+##b=7890
+##print('{:10,}'.format(a))
+##print('+{:>9,}'.format(b))
+##print('-----------')
+##print('{:>10,}'.format(a+b))
+
 a=123456
 b=7890
 print('{:10,}'.format(a))
-print('+{:>9,}'.format(b))
+print('-{:>9,}'.format(b))
 print('-----------')
-print('{:>10,}'.format(a+b))
+print('{:>10,}'.format(a-b))
