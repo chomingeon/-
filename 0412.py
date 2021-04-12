880f43e25e437e725271f4f5f70af483368aac57
chomingeon 조민건 <66959363+chomingeon@users.noreply.github.com>
2021-04-12 09:10:12 +0900

    Update 0412.py

--- 0412.py
+++ 0412.py
@@ -1,13 +1,21 @@
-##a=123456
+##a=123456##a=123456
 ##b=7890
 ##print('{:10,}'.format(a))
 ##print('+{:>9,}'.format(b))
 ##print('-----------')
 ##print('{:>10,}'.format(a+b))
 
-a=123456
-b=7890
-print('{:10,}'.format(a))
-print('-{:>9,}'.format(b))
-print('-----------')
-print('{:>10,}'.format(a-b))
+##a=123456
+##b=7890
+##print('{:10,}'.format(a))
+##print('-{:>9,}'.format(b))
+##print('-----------')
+##print('{:>10,}'.format(a-b))
+
+def numchk(a):
+    if a%2 == 1:
+        print("홀수")
+    else:
+        print("짝수")
+a=int(input("숫자입력:"))
+numchk(a)
