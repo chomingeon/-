9d1a5e18b26333ed0cc274e9d7fb5f09df93811e
chomingeon 조민건 <66959363+chomingeon@users.noreply.github.com>
2021-04-19 10:07:46 +0900

    Add files via upload

---
+++ 0419.py
@@ -0,0 +1,41 @@
+##for i in range(10 , 11, 2):
+##    print(i)
+##for f in range(5, 0):
+####    print(f)
+##for i in range(5):
+##    print(i)
+##for i in range(5 , 11):
+##    print('민건')
+##a=int(input('숫자를 입력하세요:'))
+##if a % 2 ==0:
+##    print('짝')
+##else:
+##    print('홀')
+##i = 0
+##while True:
+##    print(i)
+##    i+=1
+##    if i == 100:
+##        break
+##listA = [10,20,30,40]
+##sum = 0
+##for j in listA:
+##    sum += j
+##print(sum)
+##sum, k =0,0
+##for l in range(1,5):
+##    if k%2 ==0:
+##        continue
+##    sum+=k
+##print('합계 %d' % sum)
+code=['C','G','B','E','D','F','A','H','I']
+code.sort()
+print(code)
+del code[4]
+print(code)
+code.append('j')
+print(code)
+code.insert(4,'e')
+print(code)
+code.pop()
+print(code)
